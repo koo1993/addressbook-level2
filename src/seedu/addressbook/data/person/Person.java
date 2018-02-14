a7b3447a66af265c7664547fdc2b34c30e714328
koo1993 <chinchyekoo@hotmail.com>
2018-02-15 01:34:12 +0800

    changed parser::parseCommand to be class level// added sequenceNumber as instant-level to keep track what sequence number is the person// and nextSequenceNumber as class-level to keep track of next number//

--- src/seedu/addressbook/data/person/Person.java
+++ src/seedu/addressbook/data/person/Person.java
@@ -14,6 +14,9 @@ public class Person implements ReadOnlyPerson {
     private Phone phone;
     private Email email;
     private Address address;
+    private int sequenceNumber;
+    private static int nextSequenceNumber = 0;
+
 
     private final UniqueTagList tags;
     /**
@@ -25,6 +28,7 @@ public class Person implements ReadOnlyPerson {
         this.email = email;
         this.address = address;
         this.tags = new UniqueTagList(tags); // protect internal tags from changes in the arg list
+        sequenceNumber = ++nextSequenceNumber;
     }
 
     /**
@@ -32,6 +36,7 @@ public class Person implements ReadOnlyPerson {
      */
     public Person(ReadOnlyPerson source) {
         this(source.getName(), source.getPhone(), source.getEmail(), source.getAddress(), source.getTags());
+        sequenceNumber = ++nextSequenceNumber;
     }
 
     @Override
